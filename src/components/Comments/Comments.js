a55450cba95f604984e21f3fdf8956616fc4e7f3
reesharper <reestimharper@gmail.com>
2020-10-06 19:05:12 -0400

    completed some of the files yet still facing issues

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -9,6 +9,13 @@ const Comments = props => {
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+
+      {
+      comments.map((comment) => {
+        return 
+      })
+      }
+
     </div>
   );
 };
